3ce2375397e79dd71c8f7c13cd4ccc1749ee963d
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2024-02-03 06:48:41 -0400

    construção do chat e video chamada

--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,81 @@
-import express, {Application} from 'express';
+import express, { Application } from 'express';
 import http from 'http';
-import {Server, Socket} from "socket.io";
-
+import cors from 'cors';
+import { Server, Socket } from 'socket.io';
 class App {
-    private readonly app: Application;
-    private readonly http: http.Server;
+    private app: Application;
+    private http: http.Server;
     private io: Server;
-
     constructor() {
         this.app = express();
         this.http = new http.Server(this.app);
         this.io = new Server(this.http, {
             cors: {
-                origin: '*', // allow all
-            }
+                origin: '*',
+            },
+        });
+    }
+    public listen() {
+        this.http.listen(3333, () => {
+            console.log('Server running on port 3333');
         });
     }
 
-    public listen() {
-        this.http.listen(3333, () => {
-            console.log('Server is running on port 3333');
-        })
-    }
-
     public listenSocket() {
-        this.io.of('/streams').on('connect', this.socketEvents);
+        this.io.of('/streams').on('connection', this.socketEvents);
     }
-
     private socketEvents(socket: Socket) {
-        console.log('Socket connected back end ID ==> ' + socket.id);
+        console.log('Socket connected: ' + socket.id);
         socket.on('subscribe', (data) => {
-            console.log('número da sala', data.roomId);
+            console.log('usuario inserido na sala: ' + data.roomId);
             socket.join(data.roomId);
+            socket.join(data.socketId);
 
-            socket.on('chat', (data) => {
-                console.log('mensagem enviada ',data)
-                socket.broadcast.to(data.roomId).emit('chat', {
-                    message: data.message,
-                    userName: data.userName,
-                    time: data.time,
+            const roomsSession = Array.from(socket.rooms);
+
+            if (roomsSession.length > 1) {
+                socket.to(data.roomId).emit('new user', {
+                    socketId: socket.id,
+                    username: data.username,
                 });
-            })
-        })
+            }
+        });
+
+        socket.on('newUserStart', (data) => {
+            console.log('Novo usuario chegou', data);
+            socket.to(data.to).emit('newUserStart', {
+                sender: data.sender,
+                username: data.username,
+            });
+        });
+
+        socket.on('sdp', (data) => {
+            socket.to(data.to).emit('sdp', {
+                description: data.description,
+                sender: data.sender,
+            });
+        });
+
+        socket.on('ice candidates', (data) => {
+            socket.to(data.to).emit('ice candidates', {
+                candidate: data.candidate,
+                sender: data.sender,
+            });
+        });
+
+        socket.on('chat', (data) => {
+            socket.broadcast.to(data.roomId).emit('chat', {
+                message: data.message,
+                username: data.username,
+                time: data.time,
+            });
+        });
+
+        // socket.on('disconnect', () => {
+        //   console.log('Socket desconectado: ' + socket.id);
+        //   socket.disconnect();
+        // });
     }
 }
 
-export {App};
+export { App };
